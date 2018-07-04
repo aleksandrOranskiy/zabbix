c031751956604903553ac5d1b73da17c2e497a63
Student <student@EPBYMINW2695.minsk.epam.com>
2018-07-04 18:12:47 +0300

    Installing Zabbix using Vagrant

---
+++ zabbix-1.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+echo "Installing MariaDB..."
+yum install -y mariadb mariadb-server
+/usr/bin/mysql_install_db --user=mysql
+systemctl start mariadb
+
+echo "Creating initial database..."
+mysql -uroot <<EOF 
+create database zabbix character set utf8 collate utf8_bin;
+grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbixdb';
+EOF
+
+echo "Installing and configuring Zabbix Server..."
+yum install -y http://repo.zabbix.com/zabbix/3.2/rhel/7/x86_64/zabbix-release-3.2-1.el7.noarch.rpm
+yum install -y zabbix-server-mysql zabbix-web-mysql
+zcat /usr/share/doc/zabbix-server-mysql-*/create.sql.gz | mysql -uzabbix -pzabbixdb zabbix
+sed -i -e 's/\(.*\)\(DBPassword=\)\(.*\)/\2zabbixdb/' /etc/zabbix/zabbix_server.conf
+sed -i -e 's/\(.*\)\(php_value date\.timezone\)\(.*\)/\t\2 Europe\/Minsk/' /etc/httpd/conf.d/zabbix.conf
+sed -i -e 's/\(^Alias.*\)/#\1\n<VirtualHost *:80>\nDocumentRoot \/usr\/share\/zabbix\n<\/VirtualHost>/' /etc/httpd/conf.d/zabbix.conf
+systemctl start httpd
+systemctl start zabbix-server
+
+echo "Installing Zabbix agent..."
+yum install -y zabbix-agent
+sed -i -e 's/\(^ServerActive=.*\)/#\1/' /etc/zabbix/zabbix_agentd.conf
+systemctl start zabbix-agent
+
+
