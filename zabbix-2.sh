04cf7ed4ef3fdf7f54d53b6d7731400c685cbdfd
Student <student@EPBYMINW2695.minsk.epam.com>
2018-07-05 21:50:08 +0300

    autoregistering

--- zabbix-2.sh
+++ zabbix-2.sh
@@ -7,3 +7,15 @@ sed -i -e 's/\(^Server.*=\)\(.*\)/\1192\.168\.50\.101/' /etc/zabbix/zabbix_agent
 systemctl start zabbix-agent
 yum install -y zabbix-sender
 yum install -y zabbix-get
+yum install -y java-1.8.0-openjdk
+wget http://ftp.byfly.by/pub/apache.org/tomcat/tomcat-8/v8.5.32/bin/apache-tomcat-8.5.32.tar.gz
+mkdir /opt/tomcat
+tar xvf apache-tomcat-8.5.32.tar.gz -C /opt/tomcat --strip-components=1
+
+echo "Installing tools for Java Monitoring"
+yum install -y zabbix-java-gateway
+systemctl start zabbix-java-gateway
+systemctl enable zabbix-java-gateway
+
+echo "Autoregistering..."
+/vagrant/auto.sh
